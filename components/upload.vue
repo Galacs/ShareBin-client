adac1239d9841a3cf21e73a3630d5f865262988a
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-27 19:22:28 +0200

    french

--- components/upload.vue
+++ components/upload.vue
@@ -16,9 +16,8 @@
         <img id="file-image" src="#" alt="Preview" class="hidden" />
         <div id="start">
           <i class="fa fa-download" aria-hidden="true"></i>
-          <div>Select a file or drag here</div>
-          <div id="notimage" class="hidden">Please select an image</div>
-          <span id="file-upload-btn" class="btn btn-primary">Select a file</span>
+          <div>Choisissez un fichier ou faite glisser</div>
+          <span id="file-upload-btn" class="btn btn-primary">Choisissez un fichier </span>
         </div>
         <div id="response" class="hidden">
           <div id="messages"></div>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup>
-import FileList from '~~/components/file-list.vue';
+import FileList from '@/components/file-list.vue';
 
 </script>
 
