1b9374b3130fa3829ee0cd6977c40f2737a92573
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-03-30 14:29:03 +0200

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -1,2 +1,1 @@
-// export const api_url = "https://sharebin-server-prod.ait-younes.fr";
-export const api_url = "http://localhost:1500";
+export const api_url = "https://sharebin-server-prod.ait-younes.fr";
