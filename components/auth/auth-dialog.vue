b6b223f970c4f33b9e6ac5cca24de65539e7d892
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-04-16 15:21:33 +0200

    Css fix

--- components/auth/auth-dialog.vue
+++ components/auth/auth-dialog.vue
@@ -58,32 +58,4 @@ a {
 a:hover {
     color: rgb(70, 70, 70);
 }
-
-.login-btn {
-  font: 13px/27px Roboto,RobotoDraft,Arial,sans-serif;
-  white-space: nowrap;
-  display: inline-block;
-  outline: none;
-  font-family: Google Sans,Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
-  font-weight: 500;
-  font-size: 14px;
-  letter-spacing: 0.25px;
-  line-height: 16px;
-  margin-right: 8px;
-  margin-top: 8px;
-  min-width: 96px;
-  padding: 9px 23px;
-  text-align: center;
-  vertical-align: middle;
-  border-radius: 4px;
-  box-sizing: border-box;
-  border: 1px solid transparent;
-  margin-left: 8px;
-  background: #2b7de9;
-  box-shadow: 0 1px 2px 0 rgba(66,133,244,0.3),0 1px 3px 1px rgba(66,133,244,0.15);
-  text-decoration: none;
-  color: #fff;
-  cursor: pointer;
-}
-
 </style>
