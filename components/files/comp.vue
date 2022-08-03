7f1b1d94e6632dc030c0f701b19085f09faee717
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-03 10:35:05 +0200

    comp tailwind refactor

--- components/files/comp.vue
+++ components/files/comp.vue
@@ -1,53 +1,37 @@
 <template>
-  <div class="container">
-    <div v-for="i in files" :key="i.fileid" class="item">
-        <filesCard :file="i" @update="update" />
-      </div>
+  <div class="flex flex-row flex-wrap justify-start">
+    <div
+      v-for="i in files"
+      :key="i.fileid"
+      class="bg-red-400 p-5 outline
+      outline-2 outline-blue-900 w-2/6 min-w-[22.5rem]"
+    >
+      <filesCard :file="i" @update="update" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import fileCard from '~~/components/files/card.vue'
-
-import { apiUrl } from '@/endpoints.js';
+import { apiUrl } from '@/endpoints.js'
 import { IFile } from '@/interfaces/file'
 
-var files = ref<Array<IFile>>();
+const files = ref<Array<IFile>>()
 
-const update = async() => {
-  fetch(`${apiUrl}/account/files`,
+const update = async () => {
+  await fetch(`${apiUrl}/account/files`,
     {
       method: 'get',
-      credentials: 'include',
+      credentials: 'include'
     })
-    .then(async res => {
-      const data = await res.json();
-      data.forEach(i => {i.upload = new Date(i.upload) });
-      data.forEach(i => {i.expiration = new Date(i.expiration) });
-      files.value = data;
-      console.log(data);
+    .then(async (res) => {
+      const data = await res.json()
+      data.forEach((i) => { i.upload = new Date(i.upload) })
+      data.forEach((i) => { i.expiration = new Date(i.expiration) })
+      files.value = data
+      console.log(data)
     })
-    .catch(async e => {
-    });
+    .catch(async (e) => {
+    })
 }
-await update();
+await update()
 </script>
-
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
-
-.item {
-  background-color: tomato;
-  padding: 20px;
-  outline: 2px solid blue;
-  flex-grow: 1;
-  min-width: 22.5em;
-  width: calc(100% * 1/6);
-}
-
-</style>
