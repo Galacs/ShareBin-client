4283d75bb6ee2f4539d198728e00661b4aeaedfb
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-04 11:14:54 +0200

    file comp usefetch

--- components/files/comp.vue
+++ components/files/comp.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="flex flex-row flex-wrap justify-start">
+  <div v-if="pending">
+    Loading ...
+  </div>
+  <div v-else class="flex flex-row flex-wrap justify-start">
+    <button @click="refresh()">
+      refresh
+    </button>
     <div
       v-for="i in files"
       :key="i.fileid"
-      class="bg-red-400 p-5 outline
+      class="p-5 bg-red-400 outline
       outline-2 outline-blue-900 w-2/6 min-w-[22.5rem]"
     >
-      <filesCard :file="i" @update="update" />
+      <filesCard :file="i" @update="refresh" />
     </div>
   </div>
 </template>
@@ -17,21 +23,39 @@ import { IFile } from '@/interfaces/file'
 
 const files = ref<Array<IFile>>()
 
-const update = async () => {
-  await fetch(`${apiUrl}/account/files`,
-    {
-      method: 'get',
-      credentials: 'include'
-    })
-    .then(async (res) => {
-      const data = await res.json()
-      data.forEach((i) => { i.upload = new Date(i.upload) })
-      data.forEach((i) => { i.expiration = new Date(i.expiration) })
-      files.value = data
-      console.log(data)
-    })
-    .catch(async (e) => {
-    })
-}
-await update()
+// console.log('hello i am under the water')
+
+const { pending, data: posts, refresh } = useLazyFetch(`${apiUrl}/account/files`,
+  {
+    method: 'get',
+    credentials: 'include'
+  })
+
+watch(posts, (newPosts) => {
+  const data: Array<any> = newPosts
+  data.forEach((i) => { i.upload = new Date(i.upload) })
+  data.forEach((i) => { i.expiration = new Date(i.expiration) })
+  files.value = data
+  console.log(data)
+  // Because posts starts out null, you won't have access
+  // to its contents immediately, but you can watch it.
+})
+
+// const update = async () => {
+//   await fetch(`${apiUrl}/account/files`,
+//     {
+//       method: 'get',
+//       credentials: 'include'
+//     })
+//     .then(async (res) => {
+//       const data = await res.json()
+//       data.forEach((i) => { i.upload = new Date(i.upload) })
+//       data.forEach((i) => { i.expiration = new Date(i.expiration) })
+//       files.value = data
+//       console.log(data)
+//     })
+//     .catch(async (e) => {
+//     })
+// }
+// await update()
 </script>
