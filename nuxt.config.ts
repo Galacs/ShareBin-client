bec9372ae7e577d7358b4515308a681ef9caa9d5
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-04 12:26:11 +0200

    Added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  buildModules: [
+    '@pinia/nuxt'
+  ],
   vite: {
     server: {
       hmr: {
