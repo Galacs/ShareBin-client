2c4fed0bf96df01feb8bc466f378361c83b44d3b
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-04 14:13:56 +0200

    Comp ssr

--- components/files/comp.vue
+++ components/files/comp.vue
@@ -1,8 +1,5 @@
 <template>
-  <div v-if="pending">
-    Loading ...
-  </div>
-  <div v-else class="flex flex-row flex-wrap justify-start">
+  <div class="flex flex-row flex-wrap justify-start">
     <button @click="refresh()">
       refresh
     </button>
@@ -12,50 +9,32 @@
       class="p-5 bg-red-400 outline
       outline-2 outline-blue-900 w-2/6 min-w-[22.5rem]"
     >
-      <filesCard :file="i" @update="refresh" />
+      <filesCard :file="i" @update="refresh()" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
 import { apiUrl } from '@/endpoints.js'
-import { IFile } from '@/interfaces/file'
+import { useFilesListStore } from '@/store/filesList'
 
-const files = ref<Array<IFile>>()
+const { files } = storeToRefs(useFilesListStore())
 
-// console.log('hello i am under the water')
-
-const { pending, data: posts, refresh } = useLazyFetch(`${apiUrl}/account/files`,
+const { data, refresh } = await useFetch(`${apiUrl}/account/files`,
   {
     method: 'get',
-    credentials: 'include'
+    headers: useRequestHeaders(['cookie'])
   })
-
-watch(posts, (newPosts) => {
-  const data: Array<any> = newPosts
-  data.forEach((i) => { i.upload = new Date(i.upload) })
-  data.forEach((i) => { i.expiration = new Date(i.expiration) })
-  files.value = data
-  console.log(data)
-  // Because posts starts out null, you won't have access
-  // to its contents immediately, but you can watch it.
+watch(data, () => {
+  console.log(data.value[0])
+  data.value.forEach((i) => { i.upload = new Date(i.upload) })
+  data.value.forEach((i) => { i.expiration = new Date(i.expiration) })
+  files.value = data.value
 })
+if (process.server) {
+  await refresh()
+}
 
-// const update = async () => {
-//   await fetch(`${apiUrl}/account/files`,
-//     {
-//       method: 'get',
-//       credentials: 'include'
-//     })
-//     .then(async (res) => {
-//       const data = await res.json()
-//       data.forEach((i) => { i.upload = new Date(i.upload) })
-//       data.forEach((i) => { i.expiration = new Date(i.expiration) })
-//       files.value = data
-//       console.log(data)
-//     })
-//     .catch(async (e) => {
-//     })
-// }
-// await update()
 </script>
