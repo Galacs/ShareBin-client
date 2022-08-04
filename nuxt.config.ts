c90636360127136ff0968f9184505aceb383807a
Rémi Ait-Younes <remi.aityounes@gmail.com>
2022-08-04 11:14:35 +0200

    Enable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  vite: {
+    server: {
+      hmr: {
+        protocol: 'wss'
+      }
+    }
+  },
   css: ['~/assets/css/tailwind.css'],
   build: {
     postcss: {
